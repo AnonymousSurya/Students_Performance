8c3d2d3e2651efbdf7c1076660c79a4d28ef2bef
AnonymousSurya <suryanarayanmohapatra99@gmail.com>
2023-06-15 11:47:12 +0530

    Prediction pipeline & aws configuration done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,12 +5,6 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 
-from src.components.data_transformation import DataTransformationConfig
-from src.components.data_transformation import DataTransformation
-
-from src.components.model_trainer import ModelTrainerConfig
-from src.components.model_trainer import ModelTrainer
-
 from dataclasses import dataclass
 
 @dataclass
@@ -51,14 +45,5 @@ class DataIngestion:
         except Exception as e:
             logging.info("Exception occured")
             raise CustomException(e,sys)
-        
-if __name__=="__main__":
-    obj = DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion()
 
-    data_transformation=DataTransformation()
-    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
-
-    modeltrainer=ModelTrainer()
-    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
